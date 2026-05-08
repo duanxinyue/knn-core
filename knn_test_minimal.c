49b0eb70848337e32b12e0272517e760a0c23067
xin <2181723469@qq.com>
2026-05-08 20:17:54 +0800

    Add minimal test file

--- knn_test_minimal.c
+++ knn_test_minimal.c
@@ -9,13 +9,13 @@ static int run_basic_test() {
     KnnFeature f1[] = {10, 11};
     KnnFeature f2[] = {50, 10};
     KnnPoint train[] = {{f1, 0, dim}, {f2, 1, dim}};
-    
+
     KnnFeature test[] = {50, 9};
     KnnPoint point = {test, -1, dim};
-    
+
     KnnLabel result;
     knn_predict(train, 2, &point, 1, &result);
-    
+
     return (result == 1) ? 0 : 1;
 }
 
